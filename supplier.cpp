9e66693dc5236053bebaaa1fdf40474a3fd24251
Wiktorenergiczny <wiktorwieruszewski@gmail.com>
2026-01-12 08:20:36 +0000

    ss

---
+++ supplier.cpp
@@ -0,0 +1,16 @@
+#ifndef SUPPLIER
+#define SUPPLIER
+#include "shop.cpp"
+using namespace std;
+class Supplier {
+    private:
+    Shop shop;
+    public:
+    Supplier(Shop* shop) {
+    }
+    void deliverProduct(Product* product) {
+       
+    }
+ 
+};
+#endif
